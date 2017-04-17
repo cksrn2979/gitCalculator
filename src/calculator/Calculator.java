54bc5e6f454f9bb2f272a6f13dc23a8d855cf51d
ChangooLee <cksrn2979@gmail.com>
2017-04-18 01:06:49 +0900

    add func mul()

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,11 +8,17 @@ public class Calculator {
 	public int sub(int x, int y) {
 		return x - y;
 	}
+	
+	public int mul(int x, int y) {
+		return x * y;
+	}
+
 
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 
 		System.out.println(calc.sum(10, 20));
-		System.out.println(calc.sum(10, 5));
+		System.out.println(calc.sub(10, 5));
+		System.out.println(calc.mul(10, 3));
 	}
 }
