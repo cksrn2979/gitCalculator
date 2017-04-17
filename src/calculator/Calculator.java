8b0bed9385e397b26f0d604daa5eb2a71ab4d7e5
ChangooLee <cksrn2979@gmail.com>
2017-04-18 01:05:56 +0900

    add func sub()

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -5,9 +5,14 @@ public class Calculator {
 		return x + y;
 	}
 
+	public int sub(int x, int y) {
+		return x - y;
+	}
+
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 
 		System.out.println(calc.sum(10, 20));
+		System.out.println(calc.sum(10, 5));
 	}
 }
