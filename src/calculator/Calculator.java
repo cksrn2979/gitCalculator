6758256ffa067932ab577c804681d7361bf0801c
ChangooLee <cksrn2979@gmail.com>
2017-04-18 01:13:53 +0900

    add div()

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -13,12 +13,15 @@ public class Calculator {
 		return x * y;
 	}
 
-
+	public double div (double x, double y){
+		return (double)(x/y);
+	}
 	public static void main(String[] args) {
 		Calculator calc = new Calculator();
 
 		System.out.println(calc.sum(10, 20));
 		System.out.println(calc.sub(10, 5));
 		System.out.println(calc.mul(10, 3));
+		System.out.println(calc.div(10, 3));
 	}
 }
